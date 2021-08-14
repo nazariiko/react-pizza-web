206b3e78a6dbd46a2864033e495f470456b76a45
Nazarii Kovalenko <nazarii.dev@gmail.com>
2021-08-14 21:34:23 +0300

    Created Categories component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React from "react";
 
-import Header from './Header';
-import Button from './Button';
+import { Header, Categories } from './components'
 
 function App() {
+
   return (
     <div className="wrapper">
       <Header />
-      <Button outline>Кнопка</Button>
-      <Button>123</Button>
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <div className="categories">
-              <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-              </ul>
-            </div>
+            <Categories items={['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']} />
             <div className="sort">
               <div className="sort__label">
                 <svg
@@ -390,5 +379,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
