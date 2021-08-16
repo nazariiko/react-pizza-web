b382b94b567b040fa771b99b69b7da5770c6d529
Nazarii Kovalenko <nazarii.dev@gmail.com>
2021-08-16 14:43:41 +0300

    Added json local db with pizzas and PizzaBlock component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { render } from "@testing-library/react";
 import React from "react";
 import { Route } from "react-router-dom";
 
@@ -5,12 +6,19 @@ import { Header } from './components'
 import { Home, Cart } from './pages'
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json').then((resp) => resp.json()).then(json => {
+      setPizzas(json.pizzas)
+    })
+  }, [])
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" render={() => <Home pizzas={pizzas} />} exact />
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
